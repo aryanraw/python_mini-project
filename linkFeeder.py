ee7704b26a894cbd832a13bc8bc18c3bef3632cc
aryanraw <97694712+aryanraw@users.noreply.github.com>
2023-03-23 01:45:18 +0530

    Delete linkFeeder.py

--- linkFeeder.py
+++
@@ -1,6 +0,0 @@
-def file_to_links(filename):
-    with open(filename, 'r') as f:
-        links = f.readlines()
-        # Remove any newline characters
-        links = [link.strip() for link in links]
-        return links
